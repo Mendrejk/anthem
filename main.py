d3c4e61eac1b683312178eb16a90f2f3e9c064e3
Sebastian Łakomy <bastian.lakomy@gmail.com>
2018-11-15 09:37:24 +0100

    Importing pysine makes sounds lags go away. Without even using it anywhere.

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
 from winsound import Beep
 from time import sleep
 from math import pow
-
-# c d e f g a h
-# step = pow(2, 1/6) (1/12 for eg. a4 to a#4)
-# a4 = 440, so a4 is the 27th sound (?)
-# so it's c1 * step**26 (?)
-# ignore what's above, basically 440hz is the 1st sound on the staff (the one with added line)
-# ... i think
+import pysine
 
 root = pow(2, 1/12)
 bpm = 116
@@ -15,31 +9,22 @@ base_sound = 329.6276  # E4
 quarter_note = 0.5
 sounds = [base_sound]
 
-# E4 = 329 F5 = 698  == 14 semitones (bases)
-# 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
-#   1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28
-#   1   3   5   7 8   10    12 13    15    17    19 20    22    24    26 27
-#     2   4   6      9   11       14    16    18       21    23    25       28
-#   1 2 3 4 5 6 0 1  2 3 4  5  6  0  1  2  3  4  5  6  0  1  2   3  4  5  6  0
-
 
 for i in range(1, 14):
     sounds.append(sounds[i-1] * root)
 s = [int(x) for x in sounds]
-print(s)
 sounds = [int(x) for x in sounds]
 temp_sounds = []
 for i in range(1, len(sounds)):
     if i % 7 in [2, 4, 6] and i < 13:
         continue
     elif i % 7 in [0, 2, 4] and i >= 13:
-        print(i)
         continue
     else:
         temp_sounds.append(sounds[i])
 sounds[-1] = 698
 sounds = [int(base_sound)] + temp_sounds
-print(sounds)
+sounds[4] = 466
 
 notes = {"full_note": int(quarter_note*4000), "half_note": int(quarter_note*2000), "quarter_note": int(quarter_note*1000),
          "eight_note": int(quarter_note*500), "sixteenth_note": int(quarter_note*250)}
